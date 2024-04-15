c71e2ef1318ce9005bc98d058ee144ce5d052f49
Rafael Ruiz <rafael.ruiz.rodriguez@outlook.com>
2024-04-14 19:18:32 -0700

    Added husky configuration

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HuskyService
+{
+    public class HuskyService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
